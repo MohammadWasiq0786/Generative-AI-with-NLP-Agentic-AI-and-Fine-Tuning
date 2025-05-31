220dd3997490cbd833d9971da79eca8c8a961e62
Mohammad Wasiq <90026848+MohammadWasiq0786@users.noreply.github.com>
2025-05-31 21:02:55 +0530

    Agentic AI

---
+++ 20 Agentic AI/README.md
@@ -0,0 +1,18 @@
+# Generative AI with NLP Agentic AI and Fine Tuning
+
+![euron](https://github.com/MohammadWasiq0786/Generative-AI-with-NLP-Agentic-AI-and-Fine-Tuning/blob/main/euronone.jpeg)
+
+<center> <h1 style= "color:red"> Generative AI with NLP, Agentic AI and Fine Tuning </h1> </center>
+
+<center> <h2 style= "color:orange"> Class Date </h2> </center>
+
+<center> <h4> 31-May-2025 </h4> </center>
+
+<center> <h2 style= "color:orange"> Agentic AI </h2> </center>
+
+## Topics
+
+1. **LangChain Demo**
+2. **LangGraph Demo**
+3. **Llama-Index Demo**
+4. **CrewAI Demo**
---
+++ 20 Agentic AI/euri_llm.py
@@ -0,0 +1,18 @@
+import requests
+
+def euri_completion(messages, temperature=0.7, max_tokens=1000):
+    url = "https://api.euron.one/api/v1/euri/alpha/chat/completions"
+    EURI_API_KEY= "EURI_API_KEY" # Replace with your actual EURI API key
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": f"Bearer {EURI_API_KEY}"  # Replace with your actual EURI API key
+    }
+    payload = {
+        "messages": messages,
+        "model": "gpt-4.1-nano",
+        "max_tokens": max_tokens,
+        "temperature": temperature
+    }
+
+    response = requests.post(url, headers=headers, json=payload)
+    return response.json()["choices"][0]["message"]["content"]
